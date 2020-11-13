2ff0f4937098a39d90f3437594901648da7f94ae
HeshikNandan <heshiknndn@gmail.com>
2020-11-13 13:24:43 +0800

    Remove unwanted files

--- js/main.js
+++ js/main.js
@@ -102,43 +102,11 @@
 	
 	};
 
-	var pieChart = function() {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 4,
-			lineCap: 'butt',
-			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
-			size: 160,
-			animate: 1000
-		});
-	};
-
-	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-
-	};
-
 
 	// Loading page
 	var loaderPage = function() {
 		$(".fh5co-loader").fadeOut("slow");
 	};
-
-	var downloadResume = function() {
-		$(".download-resume").onClick(function(e){
-			e.preventDefault();
-			
-		});
-	}
 	
 
 	$(function(){
@@ -147,8 +115,6 @@
 		loaderPage();
 		fullHeight();
 		parallax();
-		// pieChart();
-		skillsWayPoint();
 	});
 
 
