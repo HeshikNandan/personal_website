a5855c4a594cf1a377192849ad216785a36af96b
HeshikNandan <heshiknndn@gmail.com>
2020-11-13 13:18:46 +0800

    Update full screen background image stretch

--- js/main.js
+++ js/main.js
@@ -25,9 +25,8 @@
 
 	
 	var fullHeight = function() {
-
+		$('.js-fullheight').css('height', $(window).height());
 		if ( !isMobile.any() ) {
-			$('.js-fullheight').css('height', $(window).height());
 			$(window).resize(function(){
 				$('.js-fullheight').css('height', $(window).height());
 			});
